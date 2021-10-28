2f2462f516f1ea13f34a6a4d1938aea5a5620fb1
abouterf <ekargosha88@gmail.com>
2021-10-28 13:34:27 +0330

    override validation

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -13,21 +13,55 @@ class Auth extends BaseController
         return view('auth/login');
     }
 
-    public function register(){
+    public function register()
+    {
         return view('auth/register');
     }
 
-    public function save(){
-        $validation = $this->validate([
+    public function save()
+    {
+        /*$validation = $this->validate([
             'name' => 'required', 
             'email' => 'required|valid_email|is_unique[users.email]',
             'password' => 'required|min_length[6]|max_length[12]',
             'passwordConfirmation' => 'required|min_length[6]|max_length[12]|matches[password]',
-
+        ]); */
+        $validation = $this->validate([
+            'name' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Your full name is required.'
+                ]
+            ],
+            'email' => [
+                'rules' => 'required|valid_email|is_unique[users.email]',
+                'errors' => [
+                    'required' => 'Your Email is required.',
+                    'valid_email' => 'Your Email is not valid.',
+                    'is_unique' => 'the Email you entered already exists.'
+                ]
+            ],
+            'password' => [
+                'rules' => 'required|min_length[6]|max_length[12]',
+                'errors' => [
+                    'required' => 'Password is required.',
+                    'min_length' => 'Password must have at least 6 characters.',
+                    'max_length' => 'Password must not have more than 12 characters.'
+                ]
+            ],
+            'passwordConfirmation' => [
+                'rules' => 'required|min_length[6]|max_length[12]|matches[password]',
+                'errors' => [
+                    'required' => 'Password is required.',
+                    'min_length' => 'Password must have at least 6 characters.',
+                    'max_length' => 'Password must not have more than 12 characters.',
+                    'matches' => 'Passwords not match together.'
+                ]
+            ]
         ]);
-        if(!$validation){
-            return view('auth/register', ['validation'=> $this->validator]);
-        }else{
+        if (!$validation) {
+            return view('auth/register', ['validation' => $this->validator]);
+        } else {
             echo "form validated successfully";
         }
     }
